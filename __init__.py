a7f4233b8a934e4509733fb58548754d049068d5
osangu <osangu012@gmail.com>
2023-01-19 15:30:18 +0900

    HEAD & NODE 구별

--- __init__.py
+++ __init__.py
@@ -1,10 +1,14 @@
 from typing import Optional
 
 
+class Head:
+
+    def __init__(self, next_node: Optional['Node'] = None):
+        self.next = next_node
+
+
 class Node:
 
     def __init__(self, data):
         self.data = data
-
-        self.previous: Optional['Node'] = None
         self.next: Optional['Node'] = None
